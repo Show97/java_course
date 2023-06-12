705ffc082374b641322b7c7e37640a6eb4c6d7e5
Show97 <ignatvinogradov@rambler.ru>
2023-06-12 17:30:51 +0500

    1

--- src/lr1/Example1.java
+++ src/lr1/Example1.java
@@ -4,9 +4,7 @@ public class Example1 {
 
     public static void main(String[] args) {
 
-        System system = null;
-
-        system.out.println("Hello world!");
+        System.out.println("Hello world!");
 
 
     }
