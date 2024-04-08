6a70986d141195d6571ba2af3d4eaaf2bc18b26f
Show97 <ignatvinogradov@rambler.ru>
2024-04-08 23:15:05 +0500

    08.04.2024

--- src/lr1/Example10.java
+++ src/lr1/Example10.java
@@ -6,7 +6,7 @@ public class Example10 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int dateNow = 2023;
+        int dateNow = 2024;
         System.out.println("Введите год своего рождения: ");
         int dateBorn = in.nextInt();
         int v = dateNow-dateBorn;
