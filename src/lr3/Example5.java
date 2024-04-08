63ed771a66fbc6e35f4790592c07b3404776355d
Show97 <ignatvinogradov@rambler.ru>
2024-04-08 22:49:48 +0500

    08.04.2024

--- src/lr3/Example5.java
+++ src/lr3/Example5.java
@@ -1,17 +1,38 @@
 package lr3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Example5 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Введите кол-во чисел в сумме: ");
         int a = in.nextInt();
+        int c = a;
+        int d = 0;
+        int e = 0;
 
+        System.out.println("Через while: ");
+        while (a > 0) {
+            int ran = random.nextInt(100);
+            if (ran % 5 == 2 || ran % 3 == 1) {
+                System.out.print(ran + ", ");
+                d = d + ran;
+                a--;
+            }
+        }
+        System.out.print("\nСумма = " + d);
 
-
-
-
+        System.out.println("\nЧерез for: ");
+            for (int i = 0; c > 0; i++) {
+                if (i % 5 == 2 || i % 3 == 1) {
+                    System.out.print(i + ", ");
+                    e = e + i;
+                    c--;
+                }
+            }
+            System.out.println("\nСумма = " + e);
     }
-
 }
